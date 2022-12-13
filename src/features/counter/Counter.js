c26894f9b4c84c9cc66157b63eadd27e4b48dbec
janekigenyi <janekigenyi@gmail.com>
2022-12-13 10:52:01 +0300

    completed

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-function Counter() {
-  // read from the Redux store
-  const items = useSelector((state) => state.items);
+function incrementCount() {
+  return { type: "count/increment" };
+}
 
-  // gives us the dispatch function to send actions to the Redux store
+function Counter() {
+  const items = useSelector((state) => state.items);
   const dispatch = useDispatch();
 
   function handleOnClick() {
-    // dispatching an action on click
-    dispatch({ type: "count/increment" });
+    dispatch(incrementCount());
   }
 
   return (
